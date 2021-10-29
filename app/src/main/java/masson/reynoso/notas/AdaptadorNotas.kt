40ee8e2f549a7fb5bb746b47fb815ababfcb0958
JorgeMasson <j_masson_reynoso@hotmail.com>
2021-10-28 19:57:45 -0700

    Clase AdaptadorNotas

---
+++ app/src/main/java/masson/reynoso/notas/AdaptadorNotas.kt
@@ -0,0 +1,33 @@
+package masson.reynoso.notas
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class AdaptadorNotas: BaseAdapter {
+    lateinit var context: Context
+    var notas = ArrayList<Nota>()
+
+    constructor(context: Context, notas: ArrayList<Nota>) {
+        this.context = context
+        this.notas = notas
+    }
+
+    override fun getCount(): Int {
+        return notas.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return notas[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+}
